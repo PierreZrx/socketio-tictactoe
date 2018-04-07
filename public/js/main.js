6289d436cac6943b6456e197314fb6e94611a427
shererere <rafal.spizewski@gmail.com>
2018-04-07 13:52:55 +0200

    Add game files

---
+++ public/js/main.js
@@ -0,0 +1,61 @@
+const socket = io('http://localhost:3000');
+
+document.querySelector('.newPlayer__button').onclick = () => {
+  let name = document.querySelector('.newPlayer__input').value;
+  socket.emit('newPlayer', name);
+  document.querySelector('.newPlayer__container').style.visibility = 'hidden';
+  document.querySelector('.newPlayer__container').style.opacity = '0';
+
+  setTimeout(function() {
+    document.querySelector('.newPlayer__container').style.display = 'none';
+    messages.style.visibility = 'visible';
+    messages.style.opacity = '1';
+    document.querySelector('.room__container').style.visibility = 'visible';
+    document.querySelector('.room__container').style.opacity = '1';
+    document.querySelector('.status__container').style.visibility = 'visible';
+    document.querySelector('.status__container').style.opacity = '1';
+  }, 300)
+}
+
+const messages = document.querySelector('.messages__container');
+socket.on('message', function(res) {
+  const messageContainerDiv = document.createElement('div');
+  messageContainerDiv.className = 'messages__message';
+
+  const messageTextSpan = document.createElement('span');
+  messageTextSpan.className = 'messages__text';
+  const messageText = document.createTextNode(res);
+  messageTextSpan.appendChild(messageText);
+
+  const timeNow = new Date();
+  const hours   = timeNow.getHours();
+  const minutes = timeNow.getMinutes();
+  const seconds = timeNow.getSeconds();
+  let timeString = "" + hours;
+  timeString  += ((minutes < 10) ? ":0" : ":") + minutes;
+  timeString  += ((seconds < 10) ? ":0" : ":") + seconds;
+
+  const messageTimeSpan = document.createElement('span');
+  messageTimeSpan.className = 'messages__time';
+  const messageTime = document.createTextNode(' [' + timeString + '] ');
+  messageTimeSpan.appendChild(messageTime);
+
+  messageContainerDiv.appendChild(messageTimeSpan);
+  messageContainerDiv.appendChild(messageTextSpan);
+  messages.appendChild(messageContainerDiv);
+});
+
+socket.on('status', (res) => {
+  document.querySelector('.status__container').innerHTML = res;
+})
+
+socket.on('draw', (res) => {
+  const square = document.querySelector('[x="'+ res.cords.x +'"][y="'+ res.cords.y +'"]');
+  square.style.background = res.type === 'x' ? 'green' : 'red';
+});
+
+document.querySelector('.board__container').addEventListener('click', (e) => {
+  const x = e.target.getAttribute('x');
+  const y = e.target.getAttribute('y');
+  socket.emit('tick', {x: x, y: y});
+}, false);
---
+++ server.js
@@ -0,0 +1,142 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use('/public', express.static('public'));
+
+server.listen(3000);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const PLAYER_LIST = [];
+const ROOM_LIST = [];
+
+class Room {
+  constructor() {
+    let id = 0;
+    for (let i = 0; i < ROOM_LIST.length; i++) {
+      let room = ROOM_LIST[i];
+      id += 1;
+      if (room.id == id) break;
+      else continue;
+    }
+    this.id = id;
+    this.players = [];
+    this.turn = null;
+    this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    ROOM_LIST.push(this);
+  }
+  checkPlayers() {
+    if (this.players.length === 2) {
+      this.startGame();
+    } else if (this.players.length === 0) {
+      this.deleteRoom();
+    } else {
+      this.board = [ [null,null,null], [null,null,null], [null,null,null]];
+      this.players[0].socket.emit('message', 'Oczekiwanie na przeciwnika...');
+    }
+  }
+  startGame() {
+    this.changeTurn();
+    this.players[0].socket.emit('message', 'Znaleziono przeciwnika! Jest nim ' + this.players[1].name + '.');
+    this.players[1].socket.emit('message', 'Znaleziono przeciwnika! Jest nim ' + this.players[0].name + '.');
+    this.players[this.turn].socket.emit('message', 'Rozpoczynasz grę!');
+  }
+  tick(id, x, y) {
+    if (id === this.players[this.turn].id && this.board[y][x] === null) {
+      this.board[y][x] = this.turn;
+      this.turn === 0
+        ? io.to(this.id).emit('draw', {type: 'x', cords: {x: x, y: y}})
+        : io.to(this.id).emit('draw', {type: 'o', cords: {x: x, y: y}});
+      this.changeTurn();
+    }
+  }
+  changeTurn() {
+    if (this.checkWin() == true) {
+      io.to(this.id).emit('status', 'Wygrał gracz: ' + this.players[this.turn].name + '.');
+      this.turn = null;
+      this.deleteRoom();
+    } else {
+      if (this.turn == null) {
+        this.turn = Math.round(Math.random());
+      } else if (this.turn == 1) {
+        this.turn = 0;
+      } else if (this.turn == 0) {
+        this.turn = 1;
+      }
+      io.to(this.id).emit('status', 'Tura gracza ' + this.players[this.turn].name + '.');
+    }
+  }
+  deleteRoom() {
+    const rIndex = ROOM_LIST.find(room => room.id === this.id);
+    ROOM_LIST.splice(rIndex, 1);
+  }
+  checkWin() {
+    let b = this.board;
+    if ((b[0][0] == b[0][1] && b[0][1] == b[0][2] && b[0][2] != null) ||
+      (b[1][0] == b[1][1] && b[1][1] == b[1][2] && b[0][2] != null) ||
+      (b[2][0] == b[2][1] && b[2][1] == b[2][2] && b[2][2] != null) ||
+      (b[0][0] == b[1][0] && b[1][0] == b[2][0] && b[2][0] != null) ||
+      (b[0][1] == b[1][1] && b[1][1] == b[2][1] && b[2][1] != null) ||
+      (b[0][2] == b[1][2] && b[1][2] == b[2][2] && b[2][2] != null) ||
+      (b[0][0] == b[1][1] && b[1][1] == b[2][2] && b[2][2] != null) ||
+      (b[0][2] == b[1][1] && b[1][1] == b[2][0] && b[2][0] != null)) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+}
+
+class Player {
+  constructor(socket, name) {
+    this.id = socket.id;
+    this.socket = socket;
+    this.name = name;
+    PLAYER_LIST.push(this);
+  }
+  distribute() {
+    const room = ROOM_LIST.length > 0 ? ROOM_LIST.find(room => room.players.length === 1) : new Room();
+    this.room = room.id;
+    this.socket.join(room.id);
+    room.players.push(this);
+    this.socket.emit('message', 'Dołączyłeś do pokoju o id ' + room.id);
+    room.checkPlayers();
+  }
+  deletePlayer() {
+    const that = this;
+
+    const pIndex = PLAYER_LIST.findIndex(player => player.id === that.id);
+    const room = ROOM_LIST.find(room => room.id === that.room);
+    const pIndexInRoom = room.players.findIndex(player => player.id === that.id);
+    PLAYER_LIST.splice(pIndex, 1);
+    room.players.splice(pIndexInRoom, 1);
+    room.players.length === 0 ? room.deleteRoom() : 1;
+  }
+}
+
+io.on('connection', (socket) => {
+  socket.on('newPlayer', function(res) {
+    let player = new Player(socket, res);
+    player.distribute();
+  });
+
+  socket.on('tick', (res) => {
+    const player = PLAYER_LIST.find(player => player.id === socket.id);
+    const room = ROOM_LIST.find(room => room.id === player.room);
+    room.tick(player.id ,res.x, res.y);
+  });
+
+  socket.on('disconnect', (res) => {
+    const player = PLAYER_LIST.find(player => player.id === socket.id);
+    player ? player.deletePlayer() : 1;
+  })
+});
